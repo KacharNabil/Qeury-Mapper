ee2240a4ce29793e2921130f1d491b1fe6111112
Nabil Kach <mac@MacBook-Pro-de-Mac.local>
2024-07-10 23:08:13 +0100

    config discovery service

---
+++ Server/keyword-repo-service/src/main/java/com/query_mapper/keyword_repo_service/KeywordRepoServiceApplication.java
@@ -0,0 +1,14 @@
+package com.query_mapper.keyword_repo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class KeywordRepoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordRepoServiceApplication.class, args);
+	}
+
+}
---
+++ Server/keyword-repo-service/src/test/java/com/query_mapper/keyword_repo_service/KeywordRepoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.query_mapper.keyword_repo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordRepoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
