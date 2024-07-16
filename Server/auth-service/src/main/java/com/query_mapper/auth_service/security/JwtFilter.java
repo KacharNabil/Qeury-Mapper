ee39a6fb8988eee58c7cdf26a5d2f1e8240ad16f
Nabil Kach <mac@MacBook-Pro-de-Mac.local>
2024-07-16 12:55:46 +0100

    create userDetails Service

--- Server/auth-service/src/main/java/com/query_mapper/auth_service/security/JwtFilter.java
+++ Server/auth-service/src/main/java/com/query_mapper/auth_service/security/JwtFilter.java
@@ -1,7 +1,23 @@
 package com.query_mapper.auth_service.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Service
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain
+    ) throws ServletException, IOException {
+
+
+    }
 }
